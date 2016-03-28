ce08685e1337ab62e30214ddc45a52cedee0cb77
Isabelle Lechat <isalechat@yahoo.com>
2016-03-28 18:35:03 +0200

    rajout de présentation.css .js .html et de l'image de fond

---
+++ presentation.js
@@ -0,0 +1,7 @@
+// $(document).ready(function(){
+//   $("#bonjour").hide();
+  // $("button").click(function(){
+  //   $("#bonjour").fadeIn(3000);
+  // });
+// });
+// alert('Merci de nous fournir autant de cours pour tester nos connaissances et nous améliorer !');
