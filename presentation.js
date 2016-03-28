83c6b1eee80788833f1ff15967e25008ab23eb68
Isabelle Lechat <isalechat@yahoo.com>
2016-03-28 20:11:20 +0200

    Correction DIV=bonjour en id=bonjour. Rajout de texte dans le html, Modification css et js.

--- presentation.js
+++ presentation.js
@@ -1,7 +1,9 @@
-// $(document).ready(function(){
-//   $("#bonjour").hide();
-  // $("button").click(function(){
-  //   $("#bonjour").fadeIn(3000);
-  // });
-// });
+$(document).ready(function(){
+  $("#bonjour").hide();
+  $("#suite").hide();
+  $("#signature").hide();
+  $("#bonjour").fadeIn(3000);
+  $("#suite").fadeIn(12000);
+  $("#signature").fadeIn(20000);
+});
 // alert('Merci de nous fournir autant de cours pour tester nos connaissances et nous améliorer !');
